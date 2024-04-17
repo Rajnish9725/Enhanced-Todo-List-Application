3b5b8ad1b96a95c2e7b5088ded35feb0d34620bd
Rajnish9725 <rajnishchaudhary17998@gmail.com>
2024-04-17 23:54:07 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "./App.css"
 const App = () => {
   return (
    <main>
-      <h1>TODO REACT + TYPESCRIPT </h1>
+      <h1>TODO APP </h1>
       <Navbar />
       <AddToDo />
       <Todos />
